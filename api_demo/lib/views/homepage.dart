2cad2b699dafdf4d74bf905d7493e4d3183ce8b2
Abymdone <abelmdone@gmail.com>
2023-09-18 12:56:18 +0300

    first commit

---
+++ api_demo/lib/views/homepage.dart
@@ -0,0 +1,81 @@
+import 'package:api_demo/services/remote.dart';
+import 'package:flutter/material.dart';
+
+
+import '../models/post.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post>? posts;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch data from Api
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteServices().getPosts();
+    setState(() {
+      isLoaded = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: Visibility(
+        visible: isLoaded,
+        // ignore: sort_child_properties_last
+        child: ListView.builder(
+          itemCount: posts?.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),
+                    color: Colors.grey[300],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          posts![index].body,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+        replacement: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
